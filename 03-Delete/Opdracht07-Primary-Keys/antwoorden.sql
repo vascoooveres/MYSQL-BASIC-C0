e0b74c6c56e00fa0ccd3e29c7fbc803339caf7e9
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2021-12-16 18:21:26 +0100

    zin

--- 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
+++ 03-Delete/Opdracht07-Primary-Keys/antwoorden.sql
@@ -1,6 +1,6 @@
 -- Opdracht 3
 DELETE FROM players WHERE id = 199304;
 -- Opdracht 4
-/*ik ben 1 id vergeten te onthouden*/
+/*ik ben 1 id vergeten te onthouden maar de opdracht is wel gelukt*/
 -- Opdracht 5
 DELETE FROM players WHERE id = 226752;
