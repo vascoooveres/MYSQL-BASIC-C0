e899916b5c058b1e8a31ebd28ad5692d2e49c999
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2021-12-16 18:08:34 +0100

    antwoorden delete sql opdrachten

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM players WHERE name="David silva";
 -- Opdracht 2 
-
+DELETE FROM players WHERE club="Willem 2";
 -- Opdracht 3
-
+DELETE FROM players WHERE club="Fc Barcelona" AND nationality="Belgium";
 -- Opdracht 4
-
+DELETE FROM players WHERE age>=38;
 -- Opdracht 5
+DELETE FROM players WHERE id=167905 OR id=169595;
